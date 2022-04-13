a8068576792e2d93e1d00cf2f9b08e70855ead02
Hossen <mdmitho199911@gmail.com>
2022-04-14 05:00:03 +0600

    style social login and component

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,4 +1,7 @@
 import React from "react";
+import google from '../../../images/google logo.png'
+import facebook from '../../../images/facebook logo.png'
+import git from '../../../images/GitH logo.png'
 
 const SocialLogin = () => {
   return (
@@ -8,8 +11,17 @@ const SocialLogin = () => {
         <p className="mt-2 px-2">or</p>
         <div style={{ height: "1px" }} className="bg-primary w-50"></div>
       </div>
-      <div className="btn btn-primary w-50">
-          Google Sing IN
+      <div className="btn btn-info d-block  mx-auto w-50 my-3" >
+        <img style={{width: '30px'}} src={google} alt=''/>
+         <span className="px-2">  Google Sing IN</span>
+      </div>
+      <div className="btn btn-info d-block  mx-auto w-50 my-3" >
+        <img style={{width: '35px'}} src={facebook} alt=''/>
+         <span className="px-2">  Facebook Sing IN</span>
+      </div>
+      <div className="btn btn-info d-block  mx-auto w-50 my-3" >
+        <img style={{width: '30px'}} src={git} alt=''/>
+         <span className="px-2">  Github Sing IN</span>
       </div>
     </div>
   );
